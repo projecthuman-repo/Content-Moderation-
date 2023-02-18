99cf79ad0c7050007aa48bee6ad1ee667a9ccf60
philiponions <hueycoy9@gmail.com>
2023-02-18 01:37:17 -0700

    Changed image/video endpoint to pass reference + added text endpoint

--- src/routes/upload.js
+++ src/routes/upload.js
@@ -3,31 +3,36 @@
 const express = require("express");
 const fileUpload = require("express-fileupload")
 const router = express.Router();
+var profanity = require('profanity-util');
 
 router.use(fileUpload())
 
-/**
- * Image Upload Endpoint
- * 
- * This endpoint handles the upload of images to the server. It expects a 
- * `multipart/form-data` request with a single image file, which should have 
- * the key "file".
- * 
- * The endpoint returns one of the following responses:
- * - 200 OK with the message "success" if the file was successfully received.
- * - 415 Unsupported Media Type with an error message if the image file is 
- * not a valid type or exceeds the maximum size limit.
- * 
- * @route POST /image
- * @param {Object} req - Express request object.
- * @param {Object} res - Express response object.
- */
+router.post("/text", function (req, res) {
+    const text = req.body.text
+    
+    // Uses profanity-util to detect profane language. check() method returns all the profane languages detected
+    const results = profanity.check(text)
+    
+    // If the results length was not empty it will return the list
+    if (results.length) {        
+        res.send({results: profanity.check(results)})
+    } else {
+
+        // if it passed, return "pass" as string
+        res.send("pass")
+    }
+})
+
 router.post("/image", function (req, res) {
     
     // Assume that this request only handles one image for now and that
     // image has the key "file"
-    const image = req.files.file
+    
+    // TODO: PARSE THE BODY TO GRAB THE REFERENCE OF THE IMAGE
+    const body = req.body
 
+    // TODO: QUERY MONGODB DATABASE BASED ON REFERENCE
+    const image = None
     const mimetype = image.mimetype
     const size = image.size / 1e6
 
@@ -55,37 +60,20 @@ router.post("/image", function (req, res) {
 
 });
 
-/**
- * Route for handling video uploads
- * 
- * This route is designed to handle the uploading of video files. It expects a 
- * `multipart/form-data` request with a single video file, which should have 
- * the key "file".
- * 
- * The endpoint performs two validity checks on the uploaded video file:
- * 1. Validates the video file's mimetype against a list of accepted types, 
- * which in this case is just "video/mp4".
- * 2. Validates the video file's size against the maximum allowed size, which 
- * is set to 100 MB in this case.
- * 
- * The endpoint returns one of the following responses:
- * - 200 OK with the message "success" if the file was successfully received.
- * - 415 Unsupported Media Type with an error message if the video file is 
- * not a valid type or exceeds the maximum size limit.
- * 
- * @route POST /video
- * @param {Object} req - Express request object.
- * @param {Object} res - Express response object.
- */
+
 router.post("/video", function (req, res) {
        
     // Assume that this request only handles one image for now and that
     // video has the key "file"
-    const video = req.files.file
+    // TODO: PARSE THE BODY TO GRAB THE REFERENCE OF THE VIDEO
+    const body = req.body
+
+    video = None
     console.log(video)
 
-    const mimetype = image.mimetype
-    const size = image.size / 1e6
+    // TODO: QUERY MONGODB DATABASE BASED ON REFERENCE OF VIDEO
+    const mimetype = video.mimetype
+    const size = video.size / 1e6
 
     const validTypes = ["video/mp4"]
     const maxVideoSize = 100 // in MB
